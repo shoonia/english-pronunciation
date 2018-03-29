6bdc2bf263e03c9f326af2977b80b80cfda7ca74
Zaytsev Sasha <zaytsev126@gmail.com>
2018-03-29 16:24:55 +0300

    button play

--- src/index.js
+++ src/index.js
@@ -4,31 +4,36 @@ import {playAudio} from './modules/audio';
 const getById = id => document.getElementById(id);
 const clearBtn = getById('btn-clear');
 const writeBtn = getById('btn-write');
+const playBtn = getById('btn-play');
 const area = getById('text-in');
 const output = getById('text-out');
 const lang = getById('lang');
 
 let accent = 'us';
 
-clearBtn.addEventListener('click', function () {
-    area.value = '';
-    output.innerHTML = '';
-});
-
+clearBtn.addEventListener('click', clearContent);
 writeBtn.addEventListener('click', writeContent);
+playBtn.addEventListener('click', selectionAndPlay);
+lang.addEventListener('change', changeLang);
 area.addEventListener('keyup', writeContent);
+output.addEventListener('dblclick', selectionAndPlay);
 
-function writeContent () {
-   output.innerHTML = createContent(area.value);
+function changeLang (event) {
+    accent = event.target.value;
 }
 
-output.addEventListener('dblclick', function () {
+function clearContent () {
+    area.value = '';
+    output.innerHTML = '';
+}
+
+function writeContent () {
+    output.innerHTML = createContent(area.value);
+}
+
+function selectionAndPlay () {
     const word = selectionWord();
     if (word) {
         return playAudio(accent, word);
     }
-});
-
-lang.addEventListener('change', function (event) {
-    accent = event.target.value;
-});
+}
